6111debc875bafbf0bcc6d6b67deac7e81600ea4
LubFotPig <lubfotpig3@gmail.com>
2018-01-31 19:00:38 -0600

    End5pm

---
+++ Assets/Player/MyReplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class MyKeyFrame
+{
+    public MyKeyFrame(float t, Vector3 p, Quaternion r)
+    {
+        time = t;
+        pos = p;
+        rot = r;
+    }
+
+    public float time;
+    public Vector3 pos;
+    public Quaternion rot;
+
+}
